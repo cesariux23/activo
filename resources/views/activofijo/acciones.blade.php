62e7cdb264fa81575f4404e0a26109f7531d5ff7
Reyna <coral.cruzazul@gmail.com>
2015-05-13 13:35:22 -0500

    agregando vista acciones des activo fijo

--- resources/views/activofijo/acciones.blade.php
+++ resources/views/activofijo/acciones.blade.php
@@ -1,4 +1,8 @@
-{!! Form::open(array('route' => array('activofijo.destroy', $o->MovTo), 'method' => 'DELETE')) !!}
-	<a class="btn btn-sm btn-info" href="{{ url('/activofijo/'.$o->MovTo.'/edit') }}"> Editar</a>
-    <button type="submit" class="btn btn-danger btn-sm">Borrar</button>
+{!! Form::open(array('route' => array('activofijo.destroy', $o->Movto), 'method' => 'DELETE')) !!}
+	<a class="btn btn-sm btn-info" href="{{ url('/'.$tipo.'/activofijo/'.$o->Movto.'/edit') }}"> Editar</a>
+	@if($o->numeroEmpleados)
+		<a class="btn btn-sm btn-default" href="{{ url('/'.$tipo.'/activofijo/'.$o->Movto) }}"> Detalles</a>
+	@else
+		<button type="submit" class="btn btn-danger btn-sm">Borrar</button>
+	@endif
 {!! Form::close() !!}
