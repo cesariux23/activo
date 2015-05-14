5259cb8d92d6e224b9b5c8accb91f92d01a3e3b8
soiracis <crisfranco3@hotmail.com>
2015-05-14 18:55:20 -0500

    actualizacion de activos federales y estatales

--- resources/views/activofijo/forms.blade.php
+++ resources/views/activofijo/forms.blade.php
@@ -130,7 +130,7 @@
 		{!!Form::text('DescOfna', @$DescOfna, array("class"=>"form-control","placeholder"=>"Departamento"))!!}
 	</div>
 
-	<div class="col-md-6">.¿El bien está actualmente localizado Físicamente?
+	<div class="col-md-6">.¿El bien está actualmente localizado físicamente?
 		<label class="radio-inline">
 			<input type="radio" name="inlineRadioOptions" id="inlineRadio1" data-toggle="modal" data-target=".bs-modal-lg"> S
 			</label>
@@ -150,7 +150,7 @@
 					<div class="modal-content">
 						<div class="modal-header">
 							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-							<h3 class="modal-title" id="myModalLabel">Movimientos al detalle histórico</h3>
+							<h3 class="modal-title" id="myModalLabel">Movimiento Histórico</h3>
 						</div>
 						<div class="modal-body">
 							<div class="row">
